e3c33accd6238b156be2feea0e364bc11a44ec29
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-06-25 13:07:28 +0100

    Update database config

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,17 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: "${process.env.DATABASE_HOST}",
+        port: "${process.env.DATABASE_PORT}",
+        database: "${process.env.DATABASE_NAME}",
+        username: "${process.env.DATABASE_USERNAME}",
+        password: "${process.env.DATABASE_PASSWORD}",
+        ssl: { rejectUnauthorized: false },
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
       options: {
         useNullAsDefault: true,

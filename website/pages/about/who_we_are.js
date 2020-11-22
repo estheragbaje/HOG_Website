3f52de8085a3832a353e70c2b24d2cc524f0ec54
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-11-22 19:30:51 +0100

    fixed responsiveness for large screen on who we are page

--- website/pages/about/who_we_are.js
+++ website/pages/about/who_we_are.js
@@ -35,21 +35,18 @@ function WhoWeAre({ abouts, departments }) {
         </Box>
       </Box>
 
-      <Box
-        py='80px'
-        // margin={['0 40px', '0 40px', '0 80px']}
-        textAlign='center'
-        bg='#F7F8F7'
-      >
-        <SubHeading color='#3AC7B1' marginBottom='16px'>
-          Get to know us better
-        </SubHeading>
-        <MainHeading fontSize={['24px', '24px', '36px']} marginBottom='28px'>
-          WHO WE ARE
-        </MainHeading>
-        <AboutUsList abouts={abouts} />
+      <Box py='80px' textAlign='center' bg='#F7F8F7'>
+        <Box maxW='1600px' margin='auto'>
+          <SubHeading color='#3AC7B1' marginBottom='16px'>
+            Get to know us better
+          </SubHeading>
+          <MainHeading fontSize={['24px', '24px', '36px']} marginBottom='28px'>
+            WHO WE ARE
+          </MainHeading>
+          <AboutUsList abouts={abouts} />
+        </Box>
       </Box>
-      <Box margin='auto' p='80px'>
+      <Box margin='auto' p='80px' maxW='1600px'>
         <FlexCard
           heading='OUR VISION'
           src='/assets/our_vision.jpeg'
@@ -87,44 +84,49 @@ function WhoWeAre({ abouts, departments }) {
         backgroundColor='gray.600'
         style={{ backgroundBlendMode: 'overlay' }}
       >
-        <Box
-          py='80px'
-          margin={['0 40px', '0 40px', '0 80px']}
-          color='white'
-          maxWidth='400px'
-        >
-          <SubHeading color='#3AC7B1' marginBottom='16px'>
-            More About us
-          </SubHeading>
-          <MainHeading fontSize={['24px', '24px', '36px']} marginBottom='28px'>
-            WHAT WE BELIEVE
-          </MainHeading>
-          <Box>
-            <Text py={4}>
-              We at RCCG House of Grace get all of our beliefs directly from
-              God, and strongly believe that only God can be held as the
-              standard of what is moral and right.
-            </Text>
-            <Text py={4}>
-              We believe Word & Spirit were the basis for the church’s formation
-              on the day of Pentecost and, therefore, should be our foundation
-              as well.{' '}
-            </Text>
-            <Link href='/about/what_we_believe'>
-              <Button
-                type='submit'
-                height={['44px', '44px', '55px']}
-                px={['20px', '20px', '30px']}
-                bg='#3AC7B1'
-                _hover={{ bg: '#1FBDA5' }}
-                _focus='teal.800'
-                fontSize={['16px', '16px', '21px']}
-                fontWeight='400'
-                color='white'
-                children='LEARN MORE'
-                my={10}
-              />
-            </Link>
+        <Box maxW='1600px' margin='auto'>
+          <Box
+            py='80px'
+            margin={['0 40px', '0 40px', '0 80px']}
+            color='white'
+            maxWidth='400px'
+          >
+            <SubHeading color='#3AC7B1' marginBottom='16px'>
+              More About us
+            </SubHeading>
+            <MainHeading
+              fontSize={['24px', '24px', '36px']}
+              marginBottom='28px'
+            >
+              WHAT WE BELIEVE
+            </MainHeading>
+            <Box>
+              <Text py={4}>
+                We at RCCG House of Grace get all of our beliefs directly from
+                God, and strongly believe that only God can be held as the
+                standard of what is moral and right.
+              </Text>
+              <Text py={4}>
+                We believe Word & Spirit were the basis for the church’s
+                formation on the day of Pentecost and, therefore, should be our
+                foundation as well.{' '}
+              </Text>
+              <Link href='/about/what_we_believe'>
+                <Button
+                  type='submit'
+                  height={['44px', '44px', '55px']}
+                  px={['20px', '20px', '30px']}
+                  bg='#3AC7B1'
+                  _hover={{ bg: '#1FBDA5' }}
+                  _focus='teal.800'
+                  fontSize={['16px', '16px', '21px']}
+                  fontWeight='400'
+                  color='white'
+                  children='LEARN MORE'
+                  my={10}
+                />
+              </Link>
+            </Box>
           </Box>
         </Box>
       </Box>
@@ -133,6 +135,7 @@ function WhoWeAre({ abouts, departments }) {
         margin='0 auto'
         textAlign='center'
         paddingX={['40px', '40px', '80px']}
+        maxW='1600px'
       >
         <Box
           textAlign='center'

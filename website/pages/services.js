5a46c8e00f3986a7eadb995bd6c42690df3a3eaa
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-11-22 20:04:07 +0100

    fixed responsiveness for larger screens on services page

--- website/pages/services.js
+++ website/pages/services.js
@@ -37,81 +37,74 @@ function Services({ services }) {
 
       <Box
         py='80px'
-        // margin={['0 40px', '0 40px', '0 80px']}
         textAlign='center'
         paddingX={['40px', '40px', '80px']}
         bg='#F7F8F7'
       >
-        <SubHeading color='#3AC7B1' marginBottom='16px'>
-          We are Passionate About Sharing the Gospel of Jesus
-        </SubHeading>
-        <MainHeading fontSize={['24px', '24px', '36px']} marginBottom='28px'>
-          ATTEND OUR WEEKLY SERVICES
-        </MainHeading>
-        <WeeklyServicesList services={services} />
+        <Box maxW='1600px' margin='auto'>
+          <SubHeading color='#3AC7B1' marginBottom='16px'>
+            We are Passionate About Sharing the Gospel of Jesus
+          </SubHeading>
+          <MainHeading fontSize={['24px', '24px', '36px']} marginBottom='28px'>
+            ATTEND OUR WEEKLY SERVICES
+          </MainHeading>
+          <WeeklyServicesList services={services} />
+        </Box>
       </Box>
 
-      <Flex
-        direction={['column', 'column', 'column', 'row']}
-        marginX={['40px', '40px', '80px']}
-        justifyContent='space-between'
-        paddingY='80px'
-      >
-        {/* <Box
+      <Box maxW='1600px' margin='auto'>
+        <Flex
+          direction={['column', 'column', 'column', 'row']}
           marginX={['40px', '40px', '80px']}
-          // px={10}
-          paddingY={16}
-        > */}
-        <Box
-          maxW='900px'
-          // minW='300px'
-
-          // px={['40px', '40px', '80px']}
+          justifyContent='space-between'
+          paddingY='80px'
         >
-          <SubHeading color='#3AC7B1' marginBottom='16px'>
-            Get to know about upcoming events
-          </SubHeading>
-          <MainHeading fontSize={['24px', '24px', '36px']}>
-            ATTEND OUR EVENTS
-          </MainHeading>
-          <Text py={4}>
-            At RCCG House of Grace, we organise and host events that would not
-            only strengthen your spiritual lives but also make you relevant and
-            a blessing to the society.
-          </Text>
-          <Text py={4}>We would love you to join us at the next events.</Text>
+          <Box maxW='900px'>
+            <SubHeading color='#3AC7B1' marginBottom='16px'>
+              Get to know about upcoming events
+            </SubHeading>
+            <MainHeading fontSize={['24px', '24px', '36px']}>
+              ATTEND OUR EVENTS
+            </MainHeading>
+            <Text py={4}>
+              At RCCG House of Grace, we organise and host events that would not
+              only strengthen your spiritual lives but also make you relevant
+              and a blessing to the society.
+            </Text>
+            <Text py={4}>We would love you to join us at the next events.</Text>
 
-          {/* </Box> */}
-          <Link href='/events '>
-            <Button
-              height={['44px', '44px', '55px']}
-              px={['20px', '20px', '30px']}
-              variant='outline'
-              _hover={{ bg: '#D0FFF8' }}
-              borderColor='#3AC7B1'
-              color='#3AC7B1'
-              _focus='teal.800'
-              fontSize={['16px', '16px', '21px']}
-              fontWeight='400'
-              marginTop={20}
-              children='VIEW ALL EVENTS'
-            />
-          </Link>
-        </Box>
-        {/* <Box size={['24px', '24px', '50px']} flexShrink='0'></Box> */}
-        <Image
-          paddingLeft={['0px', '0px', '0px', '20px']}
-          src='/assets/about_us.png'
-          alt='About us'
-          maxWidth='550px'
-          objectFit='cover'
-          flexShrink='0'
-          // objectFit='cover'
-          minWidth='400px'
-          // minHeight={['200px', '200px', '750px']}
-          display={['none', 'none', 'none', 'inline-block']}
-        />
-      </Flex>
+            <Link href='/events '>
+              <Button
+                height={['44px', '44px', '55px']}
+                px={['20px', '20px', '30px']}
+                variant='outline'
+                _hover={{ bg: '#D0FFF8' }}
+                borderColor='#3AC7B1'
+                color='#3AC7B1'
+                _focus='teal.800'
+                fontSize={['16px', '16px', '21px']}
+                fontWeight='400'
+                marginTop={20}
+                children='VIEW ALL EVENTS'
+              />
+            </Link>
+          </Box>
+          {/* <Box size={['24px', '24px', '50px']} flexShrink='0'></Box> */}
+          <Image
+            paddingLeft={['0px', '0px', '0px', '20px']}
+            src='/assets/about_us.png'
+            alt='About us'
+            maxWidth='550px'
+            objectFit='cover'
+            flexShrink='0'
+            // objectFit='cover'
+            minWidth='400px'
+            // minHeight={['200px', '200px', '750px']}
+            display={['none', 'none', 'none', 'inline-block']}
+          />
+        </Flex>
+      </Box>
+
       <Footer />
     </Box>
   );

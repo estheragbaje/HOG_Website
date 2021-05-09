c8d8d0cc2e44ce616cda81b2912f3cf07c25f526
tobechukwu <nwatu>
2021-05-08 21:33:45 -0700

    formatted give page

--- website/pages/give.tsx
+++ website/pages/give.tsx
@@ -1,10 +1,93 @@
 import React from "react";
-import { Box, Text, Image, Flex, SimpleGrid, Button } from "@chakra-ui/core";
+import {
+	Box,
+	Text,
+	Image,
+	Flex,
+	SimpleGrid,
+	Button,
+	Stack,
+} from "@chakra-ui/core";
 import Link from "next/link";
 import SubHeading from "../components/SubHeading";
 import MainHeading from "../components/MainHeading";
 import { Footer } from "../components/Footer";
 import { GetApiProvider } from "../services/providers/api-provider";
+
+const CommonFontSize = "19px";
+
+const OnlineGiving = () => {
+	const apiProvider = GetApiProvider();
+	return (
+		<Box fontSize="18px" textAlign="left" height="100%" width="100%">
+			<MainHeading
+				fontSize={["16px", "18px", "24px"]}
+				marginBottom="25px"
+				textTransform="uppercase"
+			>
+				Online Giving
+			</MainHeading>
+			<Stack
+				spacing="10px"
+				direction="column"
+				fontSize={CommonFontSize}
+				marginBottom="15px"
+			>
+				<Text>
+					You can make a recurring or one time giving.{" "}
+					<strong>Your transaction will be processed through Tithe.ly. </strong>
+				</Text>
+				<Text>
+					There is zero obligation to give, but rather an open avenue of worship
+					to connect fully with the heart of God.
+				</Text>
+			</Stack>
+			<Link href={apiProvider.give().getGivingUrl()}>
+				<a target="_blank">
+					<Button
+						height={["44px", "44px", "55px"]}
+						px={["20px", "20px", "30px"]}
+						// variant="outline"
+						variant="outline"
+						_hover={{ bg: "#61CE70" }}
+						borderColor="#61CE70"
+						background="#61CE70"
+						_focus="#61CE70"
+						color="white"
+						// _focus="teal.800"
+						fontSize={["16px", "16px", "21px"]}
+						fontWeight="400"
+						marginTop={"10px"}
+						children="GIVE ONLINE"
+					/>
+				</a>
+			</Link>
+		</Box>
+	);
+};
+
+const TextGiving = () => {
+	return (
+		<Box textAlign="left" width="100%">
+			<MainHeading
+				fontSize={["16px", "18px", "24px"]}
+				marginBottom="24px"
+				textTransform="uppercase"
+			>
+				Text
+			</MainHeading>
+			<Stack direction="column" spacing={"10px"}>
+				<Text fontSize={CommonFontSize}>
+					You can make a one time giving by
+					<strong> texting the amount HOGCC to 45888.</strong>
+				</Text>
+				<Text fontSize={CommonFontSize}>
+					For example to give $10, text <strong>$10 HOGCC</strong> to 45888
+				</Text>
+			</Stack>
+		</Box>
+	);
+};
 
 function Give() {
 	const apiProvider = GetApiProvider();
@@ -36,108 +119,32 @@ function Give() {
 
 			<Box
 				paddingTop="80px"
-				textAlign="center"
 				bg="#F7F8F7"
 				paddingX={["40px", "40px", "80px"]}
+				paddingBottom="80px"
 			>
-				<Box maxW="1600px" margin="auto">
-					<MainHeading
-						fontSize={["24px", "24px", "36px"]}
-						marginBottom="28px"
-						textTransform="uppercase"
-					>
-						Give Tithes & Offerings
-					</MainHeading>
-					<Box margin="auto" textAlign="center">
-						<Flex
-							direction={["column", "column", "column", "row"]}
-							// marginX={['40px', '40px', '80px']}
-							justifyContent="space-between"
-							paddingY="80px"
-						>
-							<Box maxW="900px" fontSize="18px" textAlign="left">
-								<MainHeading
-									fontSize={["16px", "18px", "24px"]}
-									marginBottom="28px"
-									textTransform="uppercase"
-								>
-									1. Online Giving
-								</MainHeading>
-								<Text>
-									You can make a recurring or one time giving.{" "}
-									<b>Your transaction will be processed through Tithe.ly</b>.
-									<br />
-									<br />
-									There is zero obligation to give, but rather an open avenue of
-									worship to connect fully with the heart of God.
-								</Text>
-								<Link href={apiProvider.give().getGivingUrl()}>
-									<a target="_blank">
-										<Button
-											height={["44px", "44px", "55px"]}
-											px={["20px", "20px", "30px"]}
-											variant="outline"
-											_hover={{ bg: "#D0FFF8" }}
-											borderColor="#3AC7B1"
-											color="#3AC7B1"
-											_focus="teal.800"
-											fontSize={["16px", "16px", "21px"]}
-											fontWeight="400"
-											marginTop={20}
-											children="GIVE ONLINE"
-										/>
-									</a>
-								</Link>
-							</Box>
-							<Image
-								paddingLeft={["0px", "0px", "0px", "20px"]}
-								src="/assets/our_vision.jpeg"
-								alt="About us"
-								maxWidth="550px"
-								objectFit="cover"
-								flexShrink={0}
-								minWidth="400px"
-								display={["none", "block", "block"]}
-							/>
-						</Flex>
+				<MainHeading
+					fontSize={["24px", "24px", "36px"]}
+					marginBottom="40px"
+					textTransform="uppercase"
+					textAlign="center"
+				>
+					Give Tithes & Offerings
+				</MainHeading>
+				<Flex
+					maxW="700px"
+					flexDirection="column"
+					padding="20px"
+					textAlign="justify"
+					// background="red"
+					margin="auto"
+				>
+					<OnlineGiving />
+
+					<Box marginTop="80px">
+						<TextGiving />
 					</Box>
-					<Box textAlign="center" bg="#F7F8F7">
-						<Box margin="auto" textAlign="center">
-							<Flex
-								direction={["column", "column", "column", "row"]}
-								// marginX={['40px', '40px', '80px']}
-								justifyContent="space-between"
-								paddingY="80px"
-							>
-								<Box maxW="900px" fontSize="18px" textAlign="left">
-									<MainHeading
-										fontSize={["16px", "18px", "24px"]}
-										marginBottom="28px"
-										textTransform="uppercase"
-									>
-										2. Via Text
-									</MainHeading>
-									<Text>
-										You can make a one time giving by
-										<b> texting the amount HOGCC to 45888 </b>
-										<br />
-										For example to give $10, text $10 HOGCC to 45888
-									</Text>
-								</Box>
-								<Image
-									paddingLeft={["0px", "0px", "0px", "20px"]}
-									src="/assets/sms.jpeg"
-									alt="About us"
-									maxWidth="550px"
-									objectFit="cover"
-									flexShrink={0}
-									minWidth="400px"
-									display={["none", "block", "block"]}
-								/>
-							</Flex>
-						</Box>
-					</Box>
-				</Box>
+				</Flex>
 			</Box>
 
 			<Footer />
